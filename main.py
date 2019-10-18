edaebde34cdaeb38b8b498ab44d20e44033905fe
Lindiel857 <agler857@gmail.com>
2019-10-18 14:52:03 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-pass
-#test
+def square(x, y, size, colour, angle):
+    # TODO: (Anna)
+    pass
+
+def triangle(x, y, size, colour, angle):
+    # TODO: (Timothey)
+    pass
+
+def rectangle(x, y, size, colour, angle):
+    # TODO: (Daria)
+    pass
+
+def main():
+    # TODO: (Anna) Figure 1
+    pass
+
+if __name__ == '__main__':
+    main()
