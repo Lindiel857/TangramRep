de5f2057af013c00a4a4dd70e507c6e0c95ee6d7
Lindiel <tima.vasilev.2004@mail.ru>
2019-10-24 21:02:42 +0700

    + functions

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ import turtle
 
 def square(x, y, size, colour, angle):
     # TODO: (Anna)
+    turtle.pencolor(colour)
+    turtle.fillcolor(colour)
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.rt(angle)
+    turtle.begin_fill()
+    turtle.fd(size)
+    turtle.rt(90)
+    turtle.fd(size)
+    turtle.rt(90)
+    turtle.fd(size)
+    turtle.rt(90)
+    turtle.fd(size)
+    turtle.rt(90)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.home()
+    turtle.done()
     pass
 
 def triangle(x, y, size, colour, angle):
@@ -22,15 +41,51 @@ def triangle(x, y, size, colour, angle):
     turtle.penup()
     turtle.home()
     turtle.done()
-    turtle.done()
     pass
 
 def rectangle(x, y, size, colour, angle):
     # TODO: (Daria)
+    turtle.pencolor(colour)
+    turtle.fillcolor(colour)
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.rt(angle)
+    turtle.begin_fill()
+    turtle.fd(size * 1.42)
+    turtle.rt(45)
+    turtle.fd(size)
+    turtle.rt(135)
+    turtle.fd(size * 1.42)
+    turtle.rt(45)
+    turtle.fd(size)
+    turtle.rt(135)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.home()
+    turtle.done()
     pass
 
 def main():
     # TODO: (Anna) Figure 1
+    pass
+
+    # TODO: (Anna) Figure 2
+    pass
+
+    # TODO: (Daria) Figure 3
+    pass
+
+    # TODO: (Daria) Figure 4
+    pass
+
+    # TODO: (Timothey) Figure 5
+    pass
+
+    # TODO: (Timothey) Figure 6
+    pass
+
+    # TODO: (Baba Yaga) Figure 7
     pass
 
 if __name__ == '__main__':
