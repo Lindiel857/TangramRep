beb159958599d259c439c0e6d1ee70633700683a
Lindiel857 <56710478+Lindiel857@users.noreply.github.com>
2019-11-14 17:46:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def square(x, y, size, colour, angle):
     turtle.penup()
     turtle.goto(x, y)
     turtle.pendown()
+    turtle.lt(90)
     turtle.rt(angle)
     turtle.begin_fill()
     turtle.fd(size)
@@ -26,6 +27,7 @@ def triangle(x, y, size, colour, angle):
     turtle.penup()
     turtle.goto(x, y)
     turtle.pendown()
+    turtle.lt(90)
     turtle.rt(angle)
     turtle.begin_fill()
     turtle.fd(size)
@@ -44,6 +46,7 @@ def rectangle(x, y, size, colour, angle):
     turtle.penup()
     turtle.goto(x, y)
     turtle.pendown()
+    turtle.lt(90)
     turtle.rt(angle)
     turtle.begin_fill()
     turtle.fd(size * 1.42)
@@ -81,25 +84,23 @@ def sleeping_rectangle(x, y, size, colour, angle):
     turtle.home()
 
 def main():
-    # TODO: (Timothey) Figure 1
-    triangle(-500, 200, 100, 'light slate blue', 0)
-    triangle(-600, 300, 100, 'orange', 90)
-    triangle(-500, 300, 70.5, 'lime green', 0)
-    triangle(-535.25, 335.25, 50, 'yellow', 315)
-    triangle(-500, 370.5, 50, 'blue', 45)
-    square(-500, 370.5, 50, 'cyan', 315)
-    rectangle(-535.25, 405.75, 50, 'purple', 180)
-    pass
+    turtle.speed(0)
+    
+    triangle(-500, 200 - 100, 100, 'light slate blue', 0)
+    triangle(-600, 300 - 100, 100, 'orange', 90)
+    triangle(-500, 300 - 100, 70.5, 'lime green', 0)
+    triangle(-535.25, 335.25 - 100, 50, 'yellow', 315)
+    triangle(-500, 370.5 - 100, 50, 'blue', 45)
+    square(-500, 370.5 - 100, 50, 'cyan', 315)
+    sleeping_rectangle(-500, 300 - 100, 50, 'purple', 0)
 
-    # TODO: (Timothey) Figure 2
-    triangle(-70.5, 0, 100, 'light slate blue', 45)
-    triangle(35.25, 35.35, 100, 'orange', 315)
-    triangle(-35.25, 105.75, 70.5, 'lime green', 0)
-    triangle(-35.25, 105.75, 50, 'yellow', 135)
-    triangle(0, 211.5, 50, 'blue', 45)
-    square(-25, 211.5, 50, 'cyan', 0)
-    rectangle(-35.25, 176.25, 50, 'purple', 45)
-    pass
+    triangle(-70.5, 0 + 80, 100, 'light slate blue', 45)
+    triangle(35.25, 35.35 + 80, 100, 'orange', 315)
+    triangle(-35.25, 105.75 + 80, 70.5, 'lime green', 0)
+    triangle(-35.25, 105.75 + 80, 50, 'yellow', 135)
+    triangle(0, 211.5 + 80, 50, 'blue', 225)
+    square(-25, 211.5 + 80, 50, 'cyan', 0)
+    sleeping_rectangle(-35.25, 176.25 + 80, 50, 'purple', 90)
 
     # TODO: (Anna) Figure 3
     pass
@@ -112,6 +113,8 @@ def main():
 
     # TODO: (Daria) Figure 6
     pass
+    
+    turtle.done()
 
 if __name__ == '__main__':
     main()
